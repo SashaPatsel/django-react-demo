5267705b19c2915cdaa455eb3d02c946a2196ab5
Sasha Patsel <user@Users-MacBook-Pro.local>
2020-02-09 13:16:12 -0500

    get, delete and add leads

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,26 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import store from "./store";
 import { Provider } from "react-redux";
+import Leads from "./leads/leads"
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
+      <Leads></Leads>
     </Provider>
   );
 }
--- frontend/src/reducers/leads.js
+++ frontend/src/reducers/leads.js
@@ -11,6 +11,16 @@ export default function(state = initialState, action) {
                  ...state,
                  leads: action.payload  
              };
+        case "DELETE_LEAD":
+             return {
+                ...state,
+                leads: state.leads.filter(lead => lead.id !== action.payload)
+             }
+        case "ADD_LEAD":
+        return {
+            ...state,
+            leads: [...state.leads, action.payload]
+        }
         default:
         return state;
     }
